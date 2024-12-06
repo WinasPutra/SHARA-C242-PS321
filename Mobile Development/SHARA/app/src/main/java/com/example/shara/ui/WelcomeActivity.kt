0177d32306094f115ebf201d13a339a83c6f23bc
reganputra <ramadhanregan334@gmail.com>
2024-12-06 10:41:14 +0700

    feat: add property animation

--- Mobile Development/SHARA/app/src/main/java/com/example/shara/ui/WelcomeActivity.kt
+++ Mobile Development/SHARA/app/src/main/java/com/example/shara/ui/WelcomeActivity.kt
@@ -1,7 +1,10 @@
 package com.example.shara.ui
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.shara.databinding.ActivityWelcomeBinding
 import com.example.shara.ui.auth.login.LoginActivity
@@ -17,6 +20,7 @@ class WelcomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupAction()
+        playAnimation()
     }
 
     private fun setupAction(){
@@ -29,6 +33,24 @@ class WelcomeActivity : AppCompatActivity() {
     }
 
     private fun playAnimation(){
-        // Animation
+        ObjectAnimator.ofFloat(binding.imageView,View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        val login = ObjectAnimator.ofFloat(binding.btnLogin, View.ALPHA, 1f).setDuration(100)
+        val signup = ObjectAnimator.ofFloat(binding.btnRegister, View.ALPHA, 1f).setDuration(100)
+        val desc = ObjectAnimator.ofFloat(binding.textView2, View.ALPHA, 1f).setDuration(100)
+        val title = ObjectAnimator.ofFloat(binding.textView, View.ALPHA, 1f).setDuration(100)
+
+        val together = AnimatorSet().apply {
+            playTogether(login, signup)
+        }
+
+        AnimatorSet().apply {
+            playSequentially(title, desc,together)
+            start()
+        }
     }
 }
